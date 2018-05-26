153b7b55bc4d959bdb295cf6e02ecae18e0ce1a3
Dragos Bodin <bodin.dragos@gmail.com>
2018-05-26 20:50:04 +0100

    Layed out basic setup and structure

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<div className="container">
+					<div className="row">
+						<div className="col-sm-7 col-md-6">
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+	google: {
+		families: ['Lato:400,700', 'sans-serif']
+	}
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
