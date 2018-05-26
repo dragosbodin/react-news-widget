519d4647321fc8c43d6d378285ad61d821050004
Dragos Bodin <bodin.dragos@gmail.com>
2018-05-26 22:02:38 +0100

    Jest snapshot test for news widget component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import moxios from 'moxios';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe(App, () => {
+	beforeEach(() => {
+		moxios.install(axios);
+	});
+
+	afterEach(() => {
+		moxios.uninstall(axios);
+	});
+
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<App />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
 });
